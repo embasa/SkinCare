e1d5b8a98dc8b6a24e41c90b825867db072357d6
ChrisDesigns <cchen408@gmail.com>
2015-01-10 09:17:25 -0800

    begin database site

---
+++ skin/skincitydb.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+ <head>
+    <meta charset=utf-8>
+    <link href="bootstrap.css" rel="stylesheet" type="text/css" />
+    <title>SkinCity Database</title>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+  </head>
+  <body>
+  <h1>SkinCity Database</h1>
+
+ </body>
+</html>
