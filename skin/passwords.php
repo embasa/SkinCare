aa6f5e62de47347d6e35b04bfc096d0dcd6629bf
ChrisDesigns <cchen408@gmail.com>
2015-01-09 20:25:41 -0800

    skin folder on my server

---
+++ skin/passwords.php
@@ -0,0 +1,10 @@
+<?php
+$USERS["admin"] = "hack";
+
+function check_logged(){
+     global $_SESSION, $USERS;
+     if (!array_key_exists($_SESSION["logged"],$USERS)) {
+          header("Location: login.php");
+     };
+};
+?>
---
+++ skin/php/versions.php
@@ -0,0 +1,3 @@
+<?php
+var_dump(SQLite3::version());
+?>
